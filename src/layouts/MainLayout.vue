de60f72f801950727d231024e9d13ea294b1898c
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-19 22:48:46 -0300

    refactor(src/layouts/MainLayout.vue): Limpa a topbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,10 +10,6 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
