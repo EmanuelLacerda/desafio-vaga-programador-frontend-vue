3c564cac060d1d63d97ef4b625e55380f6748eec
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-20 09:58:26 -0300

    feat(src/router/routes.js): Adiciona rota para home

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', alias: "home", name: 'home', component: () => import('pages/IndexPage.vue') },
     ]
   },
 
