e39be94a506d168f3edabaf2e63cb33aa229d71d
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-20 19:30:26 -0300

    feat(src/components/): Adiciona o componente PageTitle

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="px-4 text-4xl">{{ title }}</h1>
+</template>
+<script setup>
+defineOptions({
+  name: "PageTitle",
+});
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
