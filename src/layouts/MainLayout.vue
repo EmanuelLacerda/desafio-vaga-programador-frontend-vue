719fada740830488647b25c7214af5e6e889ad3b
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-20 19:34:32 -0300

    feat(src/layouts/MainLayout.vue): Adiciona q-page-scroller
    Este componente do Quasar foi adicionado para tornar rápido e fácil ao usuário voltar ao topo da página

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -46,6 +46,14 @@
         <component :is="Component" />
       </router-view>
     </q-page-container>
+
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn fab icon="keyboard_arrow_up" color="accent" />
+    </q-page-scroller>
   </q-layout>
 </template>
 
