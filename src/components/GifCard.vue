eaa00499662e615852e08a1f76518d463570dbd2
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-20 13:57:18 -0300

    feat(src/components): Adiciona componente GifCard

---
+++ src/components/GifCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-card-section>
+    <q-img :src="props.url">
+      <div class="absolute-bottom text-subtitle2 text-center">
+        {{ props.title }}
+      </div>
+    </q-img>
+
+    <q-card-actions
+      align="right"
+      class="q-py-xs"
+      style="background-color: rgb(225, 173, 1)"
+    >
+      <q-btn flat round color="white" icon="favorite" />
+    </q-card-actions>
+  </q-card-section>
+</template>
+<script setup>
+defineOptions({
+  name: "GifCard",
+});
+
+const props = defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
