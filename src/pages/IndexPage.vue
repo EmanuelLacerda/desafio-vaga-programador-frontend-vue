1ac6793ca5cbfe49421dc8c40729b7daa28fc309
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-20 14:04:01 -0300

    feat(src/pages/IndexPage.vue): Adiciona GifPainel no q-page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,34 +2,14 @@
   <q-page>
     <h1 class="px-4 text-4xl">GIFs Trending</h1>
 
-    <section class="md:flex flex-wrap items-end">
-      <q-card-section
-        class="md:w-1/2 xl:w-1/3 2xl:w-1/4"
-        v-for="gif in gifs"
-        :key="gif.embed_url"
-        v-bind="gif"
-      >
-        <q-img :src="gif.images.original.url">
-          <div class="absolute-bottom text-subtitle2 text-center">
-            {{ gif.title }}
-          </div>
-        </q-img>
-
-        <q-card-actions
-          align="right"
-          class="q-py-xs"
-          style="background-color: rgb(225, 173, 1)"
-        >
-          <q-btn flat round color="white" icon="favorite" />
-        </q-card-actions>
-      </q-card-section>
-    </section>
+    <GifPainel :gifs="gifs" />
   </q-page>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import gifsService from "src/services/gifs";
+import GifPainel from "src/components/GifPainel.vue";
 
 defineOptions({
   name: "IndexPage",
@@ -46,7 +26,9 @@ const getGIFsTrending = async () => {
   try {
     const data = await listTredings();
 
-    gifs.value = data;
+    gifs.value = data.map(function (gif) {
+      return { url: gif.images.original.url, title: gif.title };
+    });
   } catch (error) {
     console.log(error);
   }
