8e49b0805a24937a1da992f1154876098aac520c
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-23 22:00:45 -0300

    feat(src/stores/favorites_gifs.js): Define o store

--- src/stores/favorites_gifs.js
+++ src/stores/favorites_gifs.js
@@ -1,5 +1,12 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useFavoritesGIFsStore = defineStore('favorites_gifs', {
-    
+export const useFavoritesGIFsStore = defineStore('favorites_gifs', () => {
+    const gifs = ref([]);
+
+    function addFavoriteGif(gif_url) {
+        this.gifs.push(gif_url);
+    }
+
+    return { gifs, addFavoriteGif };
 })
