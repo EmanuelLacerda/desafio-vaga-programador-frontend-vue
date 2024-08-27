c7289e2009a0c6ed255b5d9f8a9066ff18df9afb
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-26 23:22:54 -0300

    feat(src/pages/FavoritesPage.vue): Adiciona conteúdo

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -1,8 +1,17 @@
 <template>
-  <q-page> </q-page>
+  <q-page>
+    <PageTitle title="Favoritos" />
+
+    <GifPainel :gifs="store.favorited" />
+  </q-page>
 </template>
 <script setup>
+import GifPainel from "src/components/GifPainel.vue";
+import { useGIFsStore } from "src/stores/gifs";
+
 defineOptions({
   name: "FavoritesPage",
 });
+
+const store = useGIFsStore();
 </script>
