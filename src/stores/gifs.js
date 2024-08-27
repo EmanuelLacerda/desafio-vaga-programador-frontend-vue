d9529ba3635fcca08367a44714807cb00e4bf0c3
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-08-27 13:02:58 -0300

    feat(src/stores/gifs.js): Adiciona codificação que gerencia a persistência do LocalStorage
    A partir deste commit, o array de gifs favoritados ficará salvo no LocalStorage de modo que esta informação não se perca após o fechamento/recarregamento da aba

--- src/stores/gifs.js
+++ src/stores/gifs.js
@@ -1,5 +1,7 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { LocalStorage } from 'quasar'
+
 
 export const useGIFsStore = defineStore('gifs', () => {
     const gifs = ref([]);
@@ -9,19 +11,37 @@ export const useGIFsStore = defineStore('gifs', () => {
         this.gifs = newGIFs;
     }
 
+    function insertLocalStorageInFavoritedState(){
+        if(!LocalStorage.hasItem("favoritedGIFs")){
+            LocalStorage.setItem("favoritedGIFs", [])
+        }
+
+        this.favorited = LocalStorage.getItem("favoritedGIFs")
+    }
+
     function handleActionFavorite(urlGIF){
         const findedGIF = this.gifs.find(element => element.url === urlGIF)
 
         if(findedGIF){
             findedGIF.wasFavorited = !findedGIF.wasFavorited
 
+            if(!LocalStorage.hasItem("favoritedGIFs")){
+                LocalStorage.setItem("favoritedGIFs", [])
+            }
+
+            const favoritedGIFsLocalStorage = LocalStorage.getItem("favoritedGIFs")
+
             if(this.wasTheGIFFavorited(findedGIF.url)){
                 const findedGIFIndex = this.favorited.findIndex(gif => gif.url === findedGIF.url)
 
                 this.favorited.splice(findedGIFIndex,1)
+                favoritedGIFsLocalStorage.splice(findedGIFIndex,1)
             } else{
                 this.favorited.push(findedGIF);
+                favoritedGIFsLocalStorage.push(findedGIF);
             }
+
+            LocalStorage.setItem("favoritedGIFs", favoritedGIFsLocalStorage)
         }
     }
 
@@ -29,5 +49,5 @@ export const useGIFsStore = defineStore('gifs', () => {
         return this.favorited.some(gif => gif.url === urlGIF)
     }
 
-    return { gifs, favorited, changeGIFsList, handleActionFavorite, wasTheGIFFavorited };
+    return { gifs, favorited, changeGIFsList, insertLocalStorageInFavoritedState, handleActionFavorite, wasTheGIFFavorited };
 })
