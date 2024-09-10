706136b80d934e8e521c37d412997e633e4d32f2
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-09-10 15:47:11 -0300

    feat(src/pages/IndexPage.vue): Adiciona o componente PaginationCustom

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,26 @@
-<template>
-  <q-page>
-    <PageTitle title="GIFs Trending" />
-
-    <GifPainel :gifs="store.gifs" />
-  </q-page>
-</template>
-
 <script setup>
 import { onMounted } from "vue";
+
 import gifsService from "src/services/gifs";
 
 import { useGIFsStore } from "src/stores/gifs";
 
-import GifPainel from "src/components/GifPainel.vue";
 import PageTitle from "src/components/PageTitle.vue";
+import PaginationCustom from "src/components/PaginationCustom.vue";
+import GifPainel from "src/components/GifPainel.vue";
+
+const { listTredings } = gifsService();
+const store = useGIFsStore();
+
+const getGIFsTrending = async (offset = 0) => {
+  try {
+    const data = await listTredings(offset);
+
+    store.changeGIFsList(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 defineOptions({
   name: "IndexPage",
@@ -22,17 +29,14 @@ defineOptions({
 onMounted(() => {
   getGIFsTrending();
 });
-
-const { listTredings } = gifsService();
-const store = useGIFsStore();
-
-const getGIFsTrending = async () => {
-  try {
-    const data = await listTredings();
-
-    store.changeGIFsList(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
 </script>
+
+<template>
+  <q-page>
+    <PageTitle title="GIFs Trending" />
+
+    <PaginationCustom :max="32" :changeContentPage="getGIFsTrending" />
+
+    <GifPainel :gifs="store.gifs" />
+  </q-page>
+</template>
