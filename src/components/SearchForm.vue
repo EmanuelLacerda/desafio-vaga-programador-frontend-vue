7105e06fd10123e231b456ac7e1b4807034b0299
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-09-11 13:50:05 -0300

    feat(src/components/SearchForm.vue): Adiciona formulário de pesquisa

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,6 +1,29 @@
 <script setup>
+import { ref } from "vue";
+
+const searchTerm = ref("");
+
 defineOptions({
   name: "SearchForm",
 });
 </script>
-<template></template>
+<template>
+  <q-form class="mt-8 flex justify-center">
+    <q-input
+      name="search-input"
+      type="search"
+      v-model="searchTerm"
+      aria-placeholder="Ex.: Gatos"
+      placeholder="Ex.: Gatos"
+      rounded
+      outlined
+      class="w-2/5 text-xl"
+    >
+      <template v-slot:append>
+        <q-avatar>
+          <i class="bi bi-search text-xl"></i>
+        </q-avatar>
+      </template>
+    </q-input>
+  </q-form>
+</template>
