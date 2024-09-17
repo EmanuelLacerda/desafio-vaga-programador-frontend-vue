22a8614945ae108b048cd0a53af97740d1fbb04f
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-09-17 19:29:49 -0300

    fix(Rota Favoritos): Resolve o erro na ação de desfavoritar gif
    O state favorited do store useGIFsStore é aquele que muda o value após a ação de favoritar/desfavoritar gif. Por outro lado, o state gif deste mesmo store que é o passado como prop para o componente GifPainel. Portanto, a mudança no value do primeiro não gerar um re-render do componente citado. Isto estava resultando na não atualização dos gifs mostrados em tela após a ação de desfavoritar ocorrer nesta rota.
    
    Para resolver este erro, a partir deste commit, será adicionado na page FavoritesPage um watch para o state favorited e será adicionado no store  usePaginationStore o state offset a fim de guardar a informação de qual o intervalo de gifs estava sendo mostrado antes da ação ocorrer.

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -1,20 +1,27 @@
 <script setup>
+import { onMounted, watch } from "vue";
+
 import PageTitle from "src/components/PageTitle.vue";
 import PaginationCustom from "src/components/PaginationCustom.vue";
 import GifPainel from "src/components/GifPainel.vue";
 
 import { useGIFsStore } from "src/stores/gifs";
-import { onMounted } from "vue";
+import { usePaginationStore } from "src/stores/pagination";
 
 defineOptions({
   name: "FavoritesPage",
 });
 
 const store = useGIFsStore();
+const storePagination = usePaginationStore();
 
 const getFavoritedGIFs = (offset = 0) => {
   store.changeGIFsList(store.favorited.slice(offset, offset + 15));
 };
+
+watch(store.favorited, () => {
+  getFavoritedGIFs(storePagination.offset);
+});
 
 onMounted(() => {
   getFavoritedGIFs();
--- src/stores/pagination.js
+++ src/stores/pagination.js
@@ -3,6 +3,7 @@ import { defineStore } from "pinia"
 
 export const usePaginationStore = defineStore("pagination", () => {
     const currentPage = ref(1);
+    const offset = ref(0);
 
-    return { currentPage };
+    return { currentPage, offset };
 })
